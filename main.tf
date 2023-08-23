3839093eef293b88f3cc9d5417878824734a7323
Gluppe <magnusgluppe97@gmail.com>
2023-08-23 12:02:13 +0000

    added roles and policy for nodes

--- main.tf
+++ main.tf
@@ -32,3 +32,25 @@ resource "aws_eks_cluster" "my_cluster" {
         aws_iam_role_policy_attachment.eks_policy_attachment
     ]
 }
+
+resource "aws_iam_role" "eks_node_role" {
+    name = "eks_node_role"
+    
+    assume_role_policy = jsoncode({
+        Version = "2012-10-17",
+        Statement = [
+            {
+                Action = "sts:AssumeRole",
+                Effect = "Allow",
+                Principal = {
+                    Service = "ec2.amazonaws.com"
+                }
+            }
+        ]
+    })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_policy_attachment" {
+    role = aws_iam_role.eks_node_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
