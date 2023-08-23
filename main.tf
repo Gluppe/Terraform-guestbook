2d391e7373f3a2b96f0c8f4042314d9bbed9a871
Gluppe <magnusgluppe97@gmail.com>
2023-08-23 11:54:22 +0000

    added cluster resource

--- main.tf
+++ main.tf
@@ -19,3 +19,16 @@ resource "aws_iam_role_policy_attachment" "eks_policy_attachment" {
     role = aws_iam_role.eks_role.name
     policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
+
+resource "aws_eks_cluster" "my_cluster" {
+    name = "my-cluster"
+    role_arn = aws_iam_role.eks_role.arn
+    
+    vpc_config {
+        subnet_ids = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+    }
+    
+    depends_on = [
+        aws_iam_role_policy_attachment.eks_policy_attachment
+    ]
+}
