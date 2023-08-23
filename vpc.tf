73b682ed1a74cce2e6f5bd6bdbd538d49f91f9f8
EC2 Default User <ec2-user@ip-172-31-34-24.eu-north-1.compute.internal>
2023-08-23 09:23:39 +0000

    added private and public subnets in two AZ

--- vpc.tf
+++ vpc.tf
@@ -37,3 +37,69 @@ resource "aws_subnet" "public_1" {
     }
     
 }
+
+resource "aws_subnet" "public_2" {
+    
+    # Id of the VPC where the subnet is created
+    vpc_id = aws_vpc.main.id
+    
+    # CIDR block for the subnet
+    cidr_block = "192.168.64.0/24"
+    
+    availability_zone = "eu-north-1b"
+    
+    # Required for eks, 
+    map_public_ip_on_launch = true
+    
+    # Tags to define the resource
+    tags = {
+        Name                        = "public-eu-north-1b"
+        "kubernetes.io/cluster/eks" = "shared"
+        "kubernetes.io/role/elb"    = 1
+    }
+    
+}
+
+resource "aws_subnet" "private_1" {
+    
+    # Id of the VPC where the subnet is created
+    vpc_id = aws_vpc.main.id
+    
+    # CIDR block for the subnet
+    cidr_block = "192.168.128.0/24"
+    
+    availability_zone = "eu-north-1a"
+    
+    # Required for eks, 
+    map_public_ip_on_launch = true
+    
+    # Tags to define the resource
+    tags = {
+        Name                        = "public-eu-north-1a"
+        "kubernetes.io/cluster/eks" = "shared"
+        "kubernetes.io/role/elb"    = 1
+    }
+    
+}
+
+resource "aws_subnet" "private_2" {
+    
+    # Id of the VPC where the subnet is created
+    vpc_id = aws_vpc.main.id
+    
+    # CIDR block for the subnet
+    cidr_block = "192.168.192.0/24"
+    
+    availability_zone = "eu-north-1b"
+    
+    # Required for eks, 
+    map_public_ip_on_launch = true
+    
+    # Tags to define the resource
+    tags = {
+        Name                        = "public-eu-north-1b"
+        "kubernetes.io/cluster/eks" = "shared"
+        "kubernetes.io/role/elb"    = 1
+    }
+    
+}
