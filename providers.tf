0cd457c73d699bb349ff20c849821009637174bc
Gluppe <magnusgluppe97@gmail.com>
2023-08-23 11:39:54 +0000

    added role and policy for eks cluster

--- providers.tf
+++ providers.tf
@@ -1,7 +1,18 @@
+provider "aws" {
+    # Specified in Git action, check main.yaml file for details
+}
+
 terraform {
     
     backend "s3" {
         # The backend is set up in the Git action, see main.yaml file
+    }
+    
+    required_providers {
+        aws = {
+            source = "hashicorop/aws"
+            version = "~> 5.0"
+        }
     }
     
 }
