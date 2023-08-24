a774d34d4092a35a1e4de3641c876673f502f97e
Gluppe <magnusgluppe97@gmail.com>
2023-08-24 09:58:46 +0000

    trying to create without security group

--- main.tf
+++ main.tf
@@ -71,13 +71,14 @@ resource "aws_iam_role_policy_attachment" "eks_node_policy_attachment_cni" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
+#security_groups_ids = aws_security_group.eks_worker_nodes_sg.id
+
 # The EKS cluster itself.
 resource "aws_eks_node_group" "my_node_group" {
     cluster_name = aws_eks_cluster.my_cluster.name
     node_group_name = "my-node-group"
     node_role_arn = aws_iam_role.eks_node_role.arn
     subnet_ids = [aws_subnet.private_1.id, aws_subnet.private_2.id]
-    security_groups_ids = aws_security_group.eks_worker_nodes_sg.id
     
     scaling_config {
         desired_size = 2
