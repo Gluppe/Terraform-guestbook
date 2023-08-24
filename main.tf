11f666c4902e482994fa8b7679316a6c8c39c733
Gluppe <magnusgluppe97@gmail.com>
2023-08-24 10:20:13 +0000

    switched node group from private to public subnets

--- main.tf
+++ main.tf
@@ -78,7 +78,7 @@ resource "aws_eks_node_group" "my_node_group" {
     cluster_name = aws_eks_cluster.my_cluster.name
     node_group_name = "my-node-group"
     node_role_arn = aws_iam_role.eks_node_role.arn
-    subnet_ids = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+    subnet_ids = [aws_subnet.public_1.id, aws_subnet.public_2.id]
     
     scaling_config {
         desired_size = 2
